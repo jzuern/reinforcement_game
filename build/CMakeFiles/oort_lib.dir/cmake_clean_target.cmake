0c905c23275aee2e067de40b8311373a386f94e2
Jannik Zuern <jannik.zuern@gmail.com>
2018-01-20 13:21:15 +0100

    first working version

--- build/CMakeFiles/oort_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "liboort_lib.a"
-)
--- include/util.h
+++ include/util.h
@@ -7,6 +7,57 @@
 #include "math.h"
 
 
+
+
+Eigen::MatrixXd step()
+{
+	Eigen::VectorXd observation = Eigen::VectorXd::Zero(80*80);
+	return observation;
+}
+
+
+Eigen::MatrixXd stack( std::vector<Eigen::VectorXd> xs)
+{
+	// TODO: implement correct body
+	// stacks elements of std::vector<Eigen::VectorXd> to MatrixXd
+
+	int nEntries = xs.size();
+	int entries_per_vector = xs[0].rows();
+
+	Eigen::MatrixXd result = Eigen::MatrixXd::Zero(nEntries,entries_per_vector);
+	for(int i = 0; i < nEntries; i++)
+	{
+		for(int j = 0; j < entries_per_vector; j++)
+		{
+			result(i,j) = xs[i](j);
+		}
+	}
+	return result;
+}
+
+Eigen::VectorXd stack_vector( std::vector<double> x)
+{
+	double* ptr = &x[0];
+	int size = x.size();
+	Eigen::Map<Eigen::VectorXd> stacked(ptr, size);
+
+	return stacked;
+}
+
+
+Eigen::MatrixXd prepro(sf::RenderWindow &window)
+{
+	// TODO: implement actual content
+
+	sf::Image image = window.capture();
+
+	Eigen::MatrixXd dummy = Eigen::VectorXd::Zero(50*50);
+	return dummy;
+}
+
+
+
+
 void collisionDetect(SimulatedDisk &disk_sim, Controller &disk_ctrl, float radius)
 {
 	float disk_pos_x = disk_sim.getPositionX();
@@ -33,6 +84,9 @@ void collisionDetect(SimulatedDisk &disk_sim, Controller &disk_ctrl, float radiu
 		disk_sim.setVelocity(disk_vel_x, -damp*disk_vel_y);
 	}
 }
+
+
+double stddev()
 
 
 
